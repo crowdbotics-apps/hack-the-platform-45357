77ae87fa1a082a9ffa304b67bc0f84bda2e41177
Crowdbotics <team@crowdbotics.com>
2024-01-11 19:38:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hack-the-platform-45357",
+  url: "https://hack-the-platform-45357.botics.co",
+  api: "https://hack-the-platform-45357.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
